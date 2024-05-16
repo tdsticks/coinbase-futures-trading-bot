fc3624574aae9ce733411067edfa30d452ddc58d
tdsticks <tdsticks@gmail.com>
2024-05-16 19:46:47 -0400

    Adding a Dashboard template to the admin view.

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,6 +1,9 @@
 from flask import redirect, url_for, request
 from flask_admin import expose, AdminIndexView
 from flask_login import current_user
+from app.models import db
+from app.models.signals import AuroxSignal, FuturePriceAtSignal
+from app.models.futures import FuturePosition, FuturesOrder
 
 
 class MyAdminIndexView(AdminIndexView):
@@ -8,4 +11,15 @@ class MyAdminIndexView(AdminIndexView):
     def index(self):
         if not current_user.is_authenticated and not current_user.is_superuser:
             return redirect(url_for('login', next=request.url))
-        return super(MyAdminIndexView, self).index()
+        # return super(MyAdminIndexView, self).index()
+
+        # Query data for the dashboard
+        latest_signals = AuroxSignal.query.order_by(AuroxSignal.timestamp.desc()).limit(17).all()
+        latest_positions = FuturePosition.query.order_by(FuturePosition.expiration_time.desc()).limit(1).all()
+        latest_orders = FuturesOrder.query.order_by(FuturesOrder.created_time.desc()).limit(10).all()
+
+        # Render the custom dashboard template
+        return self.render('admin/dashboard.html',
+                           latest_signals=latest_signals,
+                           latest_positions=latest_positions,
+                           latest_orders=latest_orders)
